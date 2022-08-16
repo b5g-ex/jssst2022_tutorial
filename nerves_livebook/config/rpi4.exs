5dbae8f193a19b095ab88eef26fcba99f8052977
NISHIUCHI Kazuma <nishiuchi.kazuma@gmail.com>
2022-08-17 08:44:04 +0900

    add: eth0 static ip settings for rpi4

--- nerves_livebook/config/rpi4.exs
+++ nerves_livebook/config/rpi4.exs
@@ -5,7 +5,15 @@ import Config
 config :vintage_net,
   config: [
     {"usb0", %{type: VintageNetDirect}},
-    {"eth0", %{type: VintageNetEthernet, ipv4: %{method: :dhcp}}},
+    {"eth0",
+     %{
+       type: VintageNetEthernet,
+       ipv4: %{
+         method: :static,
+         address: "192.168.5.55",
+         netmask: "255.255.255.0"
+       }
+     }},
     {"wlan0", %{type: VintageNetWiFi}}
   ]
 
